f907ec7db972fb3bbc0e6bc56a16ada86d29ce8a
Sherry Xu <sherryxu826@gmail.com>
2021-01-08 22:59:59 -0500

    added colors that only meng nan will love.

--- options.js
+++ options.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let page = document.getElementById('buttonDiv');
-const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
+const kButtonColors = ['#ffa7ae', '#ffc0c5', '#ffdadd', '#fff3f4'];
 function constructOptions(kButtonColors) {
   for (let item of kButtonColors) {
     let button = document.createElement('button');
